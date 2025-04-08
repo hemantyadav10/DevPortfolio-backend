29d2d4ebfba621dd4f455eaa27f60279ef5df4ed
Hemant <yad.heman@gmail.com>
2025-04-09 01:42:46 +0530

    feat: add ApiError, ApiResponse, and asyncHandler utility classes

---
+++ src/utils/apiError.js
@@ -0,0 +1,26 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    stack = '',
+    data = null
+  ) {
+    super(message);
+
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = false;
+    this.error = errors;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
+
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,11 @@
+function asynHandler(fn) {
+  return async (req, res, next) => {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export { asynHandler }
